5419cba30205210a38b1b99d673b9a0b3343afa7
DanielUnam09 <danielac9@ciencias.unam.mx>
2026-08-20 17:53:48 -0600

    Se agregaron los ejercicios del 4 al 6.

--- practica01/Laboratorio01.hs
+++ practica01/Laboratorio01.hs
@@ -8,7 +8,31 @@ distanciaOrigen x y = sqrt((x)^2 + (y)^2)
 sumaCuadradoPares :: [Int] -> Int
 sumaCuadradoPares xs =  sum (map ( ^2) (filter even xs))
 
--- Ejercicio 3 
+-- Ejercicio 3 Aplica una función 3 veces al mismo valor. 
 
 aplicaTresVeces :: (a -> a) -> a -> a 
 aplicaTresVeces f x = (f (f (f x)))
+
+-- Ejercicio 4 Saca el primedio de los parametros de entrada y los usa para sacar la varianza 
+varianza2 :: Double -> Double -> Double 
+varianza2 datoUno datoDos = 
+          let mediaLocal  =  (datoUno + datoDos) / 2
+          in ((datoUno - mediaLocal)^2 + (datoDos - mediaLocal)^2)
+
+-- Ejercicio 5 Usa guards para selecionar la temperatura correcta.
+clasificaTemperatura :: Int -> String
+clasificaTemperatura temperatura 
+                       | (temperatura <= 0) = "frio extremo"
+                       | (temperatura > 0 && temperatura <= 18) = "frio"
+                       | (temperatura > 18 && temperatura <= 23) = "templado"
+                       | (temperatura > 23 && temperatura <= 32) = "calido" 
+                       | otherwise = "calor extremo"
+
+
+-- Ejercicio 6 Separador de elementos usando recuersión 
+intercala :: a -> [a] -> [a]
+intercala nuevoVal []  = []
+intercala nuevoVal [x] = [x]
+intercala nuevoVal (y:ys) =  y :  nuevoVal : intercala  nuevoVal ys
+ 
+
